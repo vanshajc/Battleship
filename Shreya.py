9cc875b490d693feff8f3b42efa60348e591df8d
18chowdhary <main@ShreyaChowdhary.fios-router.home>
2016-12-30 21:43:54 -0500

    fixing shreya class

--- Shreya.py
+++ Shreya.py
@@ -1,5 +1,6 @@
 #Shreya.py
 import socket 
+import time 
 HOST = '192.168.1.174'    # The remote host
 PORT = 5003              # The same port as used by the server
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -7,7 +8,9 @@ print ('Connecting...')
 s.connect((HOST, PORT))
 data = s.recv(1024)
 
-print("Data") 
-s.sendall('2,2 7,7') 
-s.sendall('1,0 5,1')
-s.sendall('3,4 6,7')
+print(data) 
+s.send(('2,2 2,7').encode())
+time.sleep(0.1)
+s.send(('1,0 5,0').encode())
+time.sleep(0.1)
+s.send(('3,4 6,4').encode())
