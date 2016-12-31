52f9425f8a9ed60d376c57f5de3cebd3263537f1
vchowdhary <vanshikachowdhary@gmail.com>
2016-12-30 19:31:20 -0500

    fixed board class

--- board.py
+++ board.py
@@ -1,18 +1,33 @@
+import sys
 class Board:
-    '''8x8, '''
-    def __init__(self):
-        self.f = open("board.txt", "w")
+    '''8x8, 2 ships each of lengths 1, 2, 3, 4
+        1 = hit, 2 = sink, 0 = nothing, 9 = boat'''
+    def __init__(self, ships):
         #self.f.write("test")
-        row = "0 0 0 0 0 0 0 0"
-        self.cols = [[] for i in range(8)]
-        for i in range(8):
-            self.cols[i].append(row)
-        for i in range(len(self.cols)):
-            for j in range(len(self.cols[i])):
-                self.f.write(self.cols[i][j])
-            self.f.write("\n")
-    def setup(self):
-        print()
-    def update(self):
-        print()
-board = Board()
+        self.board = [[0 for i in range(8)] for j in range(8)]
+        self.ships = ships
+    def update(self, pos):
+        for ship in self.ships:
+            for pos_i in ship:
+                if pos[0] == pos_i[0] and pos[1] == pos_i[1]:
+                    ship.pop(ship.index(pos_i))
+                    ship.append((pos_i[0], pos_i[1], "hit"))
+                    self.board[pos[0]][pos[1]] = "1"
+            sunk = True
+            for pos_i in ship:
+                if pos_i[2] == "not hit": sunk = False
+            if sunk == True:
+                for pos_i in ship:
+                    self.board[pos_i[0]][pos_i[1]] = "2"
+
+    def print(self):
+        for i in range(len(self.board)):
+            for j in range(len(self.board[0])):
+                sys.stdout.write(str(self.board[i][j]) + " ")
+            print("\n")
+    def getBoard(self):
+        return board
+board = Board([[(0, 0, 'not hit')], [(0, 2, 'not hit'), (1, 2, 'not hit')], [(3, 3, 'not hit')]])
+board.update((0, 2))
+board.update((1, 2))
+board.print()
